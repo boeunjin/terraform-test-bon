63a4050e8de5cc367526cc19f68d2503390f9ee7
boeunjin <boeunjin@lguplus.co.kr>
2022-06-24 17:13:49 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "vpc" {
+    cidr_block = var.pvc_cidr
+}
+
+resource "aws_subnet" "subnet" {
+    availability_zone = "ap-northeast-2a"
+    cidr_block = var.subnet_cidr
+    vpc_id = aws_vpc.vpc.id
+    map_public_ip_on_launch = true
+}
+
+resource "aws_instance" "ec2" {
+    ami = var.instance_ami
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.subnet.id
+    associate_public_ip_address = true
+}
---
+++ vpc.tf
@@ -0,0 +1,3 @@
+resource "aws_vpc" "main" {
+    cidr_block = "10.1.0.0/24"
+}
