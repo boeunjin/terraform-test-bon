ca3c9a91008fee89cc61342abb9afc70306999a4
Boeun <boeunjin@lguplus.co.kr>
2022-07-26 14:10:20 +0900

    3rd test

--- mian.tf
+++ mian.tf
@@ -9,6 +9,13 @@ resource "aws_subnet" "subnet" {
     map_public_ip_on_launch = true
 }
 
+resource "aws_subnet" "subnet" {
+    availability_zone = "ap-northeast-2c"
+    cidr_block = var.subnet_cidr
+    vpc_id = aws_vpc.vpc.id
+    map_public_ip_on_launch = true
+}
+
 # resource "aws_instance" "ec2" {
 #     ami = var.instance_ami
 #     instance_type = var.instance_type
